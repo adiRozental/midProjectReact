60bb5b7f8b7f1d0c27bec06220e0308b1bbe348e
adiRozental <adirosental1@gmail.com>
2024-04-26 16:48:01 +0300

    all

---
+++ src/components/Todo.jsx
@@ -0,0 +1,23 @@
+// src/components/Todo.jsx
+import React from 'react';
+
+const Todo = ({ todo, onCompleteTodo }) => {
+  return (
+    <div style={{ 
+        border:  '2px solid pink', 
+        padding: '10px', 
+        margin: '10px',  borderRadius: '10px'
+      }} >        
+        <label style={{ marginRight: '10px' }}><u>Title:</u> </label> <span>{todo.title}</span><br />
+        <label style={{ marginRight: '10px' }}><u>Completed:</u> </label> <span>{todo.completed ? 'True' : 'False'}</span>
+        {!todo.completed && (
+            <button onClick={() => onCompleteTodo(todo.id)} style={{ marginLeft: '30px' }}>
+            Mark Completed
+            </button>
+        )}
+        
+    </div>
+  );
+};
+
+export default Todo;
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,23 +1,36 @@
 // src/components/TodoList.js
 import React from 'react';
+import Todo from './Todo';
 
-const TodoList = ({ todos, onCompleteTodo }) => {
+const TodoList = ({ todos, onCompleteTodo, userId }) => {
   console.log("whattt", todos)
   return (
-    <div>
-      <h2>Todo List</h2>
-      <ul>
-        {todos.map(todo => (
-          <li key={todo.id}>
-            {todo.title} {todo.completed ? '(Completed)' : '(Uncompleted)'}
-            {!todo.completed && (
-              <button onClick={() => onCompleteTodo(todo.id)}>
-                Mark Completed
-              </button>
-            )}
-          </li>
-        ))}
-      </ul>
+    <div style={{ 
+      margin: '10px'
+    }}>
+
+      <div style={{ display: 'flex' }}>
+          <div style={{flex: 3, padding: '10px'}}>
+            <h3>Todo List - User {userId} :</h3>
+          </div>
+        
+          <div style={{flex: 1, margin: '35px'}}>
+            <button  onClick={onCompleteTodo}>Add</button>
+          </div>  
+      </div>  
+      {/* <center>
+        <h2>Todo List</h2>
+        <h3>User: {userId}</h3>
+      </center> */}
+      <div style={{ border: '2px solid black',  borderRadius: '40px'} }>
+        <ul>
+          {todos.map(todo => (
+            <li key={todo.id}>
+              <Todo todo={todo} onCompleteTodo={onCompleteTodo} />
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
